bad02e32efdff10a1889e209bd8fbe9832457376
Richard Ball <webalation@gmail.com>
2020-08-20 07:30:23 -0400

    Filtering: displaying items

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -60,10 +60,12 @@ class Movies extends Component {
 
         return (
             <div className="row">
-                <div className="col-2">
+                <div className="col-3">
                     <ListGroup 
                         items={this.state.genres} 
-                        onItemSelect={this.handleGenreSelect} 
+                        onItemSelect={this.handleGenreSelect}
+                        textProperty="name"
+                        valueProperty="_id" 
                     />
                 </div>
                 <div className="col">
