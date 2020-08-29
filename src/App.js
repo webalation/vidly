3c6f1475448a0778b1c7187e597855d9b044d37b
Richard Ball <webalation@gmail.com>
2020-08-29 08:07:20 -0400

    Linked to MovieForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 import "./App.css";
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <NavBar />
         <main role="main" className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
