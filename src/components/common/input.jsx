c079e027db6ff57165dedf270a1912ebc2b810a0
Richard Ball <webalation@gmail.com>
2020-08-31 06:02:56 -0400

    Displaying validation errors

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { values } from "lodash";
 
-const Input = ({ name, label, value, onChange, username }) => {
+const Input = ({ name, label, value, onChange, error, username }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
@@ -15,6 +15,7 @@ const Input = ({ name, label, value, onChange, username }) => {
         type="text"
         className="form-control"
       />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -23,8 +23,7 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors: errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     // Call the server
@@ -38,7 +37,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -49,12 +48,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
