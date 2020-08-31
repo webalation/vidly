ab2a57a2a0bd2497c86895496f585db2cf0c39fd
Richard Ball <webalation@gmail.com>
2020-08-31 07:57:17 -0400

    Validating a field using Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,7 +19,7 @@ class LoginForm extends Component {
     if (!error) return null;
 
     const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
+    for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
   };
 
@@ -35,14 +35,10 @@ class LoginForm extends Component {
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-      // ...
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-      // ...
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
