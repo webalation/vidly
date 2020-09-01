cf79c0d69123f915e89646b24c6ab1262b228ea3
Richard Ball <webalation@gmail.com>
2020-09-01 09:27:53 -0400

    Extracting helper rendering methods

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./input";
 import Joi from "joi-browser";
 
 class Form extends Component {
@@ -44,6 +45,29 @@ class Form extends Component {
     data[input.name] = input.value;
     this.setState({ data: data, errors: errors });
   };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = "text") {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 }
 
 export default Form;
--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import { values } from "lodash";
 
-const Input = ({ name, label, value, onChange, error, username }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        autoFocus
-        value={value}
-        onChange={onChange}
-        name={name}
-        ref={username}
-        id={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
