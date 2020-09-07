ba39b1a5d7f76ead625695633628968843f357b8
Richard Ball <webalation@gmail.com>
2020-09-07 08:18:49 -0400

    Refactored componentDidMount method

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -32,14 +32,16 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+
       const { data: movie } = await getMovie(movieId);
       this.setState({
         data: this.mapToViewModel(movie),
@@ -48,6 +50,11 @@ class MovieForm extends Form {
       if (error.response && error.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
