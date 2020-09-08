8fdcde597ae73e7359b139257dd04e1536ca7a64
Richard Ball <webalation@gmail.com>
2020-09-08 08:27:49 -0400

    Stored JSON web token in local storage

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -17,7 +17,9 @@ class LoginForm extends Form {
   doSubmit = async () => {
     try {
       const { data } = this.state;
-      await login(data.username, data.password);
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem("token", jwt);
+      this.props.history.push("/");
     } catch (error) {
       if (error.response && error.response.status === 400) {
         const errors = { ...this.state.errors };
