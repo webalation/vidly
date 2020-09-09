be5a50bfd59c310c21f29634ca7422b8fa084d82
Richard Ball <webalation@gmail.com>
2020-09-09 07:03:43 -0400

    Displaying the current user on navBar

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -22,7 +22,7 @@ class Register extends Form {
     try {
       const response = await userService.register(this.state.data);
       localStorage.setItem("token", response.headers["x-auth-token"]);
-      this.props.history.push("/");
+      window.location = "/";
     } catch (error) {
       if (error.response && error.response.status === 400) {
         const errors = { ...this.state.errors };
