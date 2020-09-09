e5d0c6db0865bbc27b55414c671ece9905dbb35c
Richard Ball <webalation@gmail.com>
2020-09-09 05:44:16 -0400

    Completed logging in the user upon registration

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -20,7 +20,9 @@ class Register extends Form {
 
   doSubmit = async () => {
     try {
-      await userService.register(this.state.data);
+      const response = await userService.register(this.state.data);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      this.props.history.push("/");
     } catch (error) {
       if (error.response && error.response.status === 400) {
         const errors = { ...this.state.errors };
