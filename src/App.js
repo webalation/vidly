ff719bfdb03c28e9fa6e4d188d87c49d3c5ad2cd
Richard Ball <webalation@gmail.com>
2020-09-10 08:18:38 -0400

    Showing or hiding elements based on the user

--- src/App.js
+++ src/App.js
@@ -33,7 +33,10 @@ class App extends Component {
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies}></Route>
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={this.state.user} />}
+            />
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
             <Route path="/not-found" component={NotFound}></Route>
